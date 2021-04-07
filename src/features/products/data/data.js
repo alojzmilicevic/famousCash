2bba8bb2dcb5905c9fea757487e760709d35e992
Alojz Milicevic <alojz.milicevic@gmail.com>
2021-04-07 14:47:05 +0200

    Removed assestsloader, wasnt good. Added more products

--- src/features/products/data/data.js
+++ src/features/products/data/data.js
@@ -1,23 +1,55 @@
-import getImage, { ImageTypes } from "../../../assets/assetsLoader";
+import milk from '../../../assets/products/milk.jpg'
+import rice from '../../../assets/products/rice.jpg'
+import wine from '../../../assets/products/wine.jpg'
+import fitness from '../../../assets/products/fitness.jpg'
+import nike from '../../../assets/products/nike.jpg'
+import ps4 from '../../../assets/products/ps4.jpg'
+import laptop from '../../../assets/products/laptop.jpg'
+import rent from '../../../assets/products/rent.jpg'
+import bengal from '../../../assets/products/bengal.jpg'
+import wimbledon from '../../../assets/products/wimbledon.jpg'
+import honda from '../../../assets/products/honda.jpg'
+import courtside from '../../../assets/products/courtside.jpg'
+import audi from '../../../assets/products/audi.jpg'
+import canalboat from '../../../assets/products/canalboat.jpg'
+import yacht from '../../../assets/products/yacht.jpg'
 
 export const productData = [
-  { id: 0, img: getImage(ImageTypes.MILK), label: 'Milk', price: 1 },
-  { id: 1, img: getImage(ImageTypes.RICE), label: 'Rice', price: 4 },
-  { id: 2, img: getImage(ImageTypes.WINE), label: 'Wine', price: 12 },
-  { id: 3, img: getImage(ImageTypes.FITNESS), label: 'Fitness Club, monthly fee', price: 37 },
-  { id: 4, img: getImage(ImageTypes.NIKE), label: '1 Pair of Nike Running Shoes', price: 75 },
-  { id: 5, img: getImage(ImageTypes.PS4), label: 'Gaming Console', price: 299 },
-  { id: 6, img: getImage(ImageTypes.LAPTOP), label: 'Laptop - gaming', price: 748.00 },
-  { id: 7, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 8, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 9, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 10, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 11, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 12, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 13, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 14, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 15, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 16, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 17, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
-  { id: 18, img: getImage(ImageTypes.RENT), label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
+  { id: 0, img: milk, label: 'Milk', price: 1 },
+  { id: 1, img: rice, label: 'Rice', price: 4 },
+  { id: 2, img: wine, label: 'Wine', price: 12 },
+  { id: 3, img: fitness, label: 'Fitness Club, monthly fee', price: 37 },
+  { id: 4, img: nike, label: '1 Pair of Nike Running Shoes', price: 75 },
+  { id: 5, img: ps4, label: 'Gaming Console', price: 299 },
+  { id: 6, img: laptop, label: 'Laptop - gaming', price: 748.00 },
+  { id: 7, img: rent, label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
+  { id: 8, img: bengal, label: 'Bengal Cat', price: 5000 },
+  { id: 9, img: wimbledon, label: 'Wimbledon Finals Tickets', price: 10000 },
+  { id: 10, img: '', label: 'Aged Whiskey', price: 11000 },
+  { id: 11, img: '', label: '??', price: 13000 },
+  { id: 12, img: '', label: '??', price: 18500 },
+  { id: 13, img: honda, label: 'Honda Civic', price: 26000 },
+  { id: 13, img: '', label: '??', price: 35000 },
+  { id: 14, img: audi, label: '2020 Audi TT', price: 50000 },
+  { id: 15, img: canalboat, label: 'Canal Boat', price: 100000 },
+  { id: 16, img: courtside, label: 'Lakers Court Side Season Tickets', price: 200000 },
+  { id: 17, img: '', label: '??', price: 0 },
+  { id: 18, img: '', label: '??', price: 0 },
+  { id: 19, img: '', label: '??', price: 0 },
+  { id: 20, img: '', label: '??', price: 0 },
+  { id: 21, img: yacht, label: 'Luxury Yacht', price: 14000000 },
+  { id: 22, img: '', label: '??', price: 0 },
+  { id: 23, img: '', label: '??', price: 0 },
+  { id: 24, img: '', label: '??', price: 0 },
+  { id: 25, img: '', label: '??', price: 0 },
+  { id: 26, img: '', label: '??', price: 0 },
+  { id: 27, img: '', label: '??', price: 0 },
+  { id: 28, img: '', label: '??', price: 0 },
+  { id: 29, img: '', label: '??', price: 0 },
+  { id: 30, img: '', label: '??', price: 0 },
+  { id: 31, img: '', label: '??', price: 0 },
+  { id: 32, img: '', label: '??', price: 0 },
+  { id: 33, img: '', label: '??', price: 0 },
+  { id: 34, img: '', label: '??', price: 0 },
+  { id: 35, img: '', label: '??', price: 0 },
 ]
