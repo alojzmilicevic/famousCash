27e26b84250804287769811e7dea666f21ef2c66
Alojz Milicevic <alojz.milicevic@gmail.com>
2021-04-07 17:47:46 +0200

    Add more options

--- src/features/products/data/data.js
+++ src/features/products/data/data.js
@@ -13,6 +13,21 @@ import courtside from '../../../assets/products/courtside.jpg'
 import audi from '../../../assets/products/audi.jpg'
 import canalboat from '../../../assets/products/canalboat.jpg'
 import yacht from '../../../assets/products/yacht.jpg'
+import whiskey from '../../../assets/products/whiskey.jpg'
+import penthouse from '../../../assets/products/penthouse.jpg'
+import diamondshoes from '../../../assets/products/diamondshoes.jpg'
+import island from '../../../assets/products/al-marjan-island.jpg'
+import iss from '../../../assets/products/iss.jpg'
+import luxembourg from '../../../assets/products/luxembourg.jpg'
+import collage from '../../../assets/products/collage.jpg'
+import netflix from '../../../assets/products/netflix.jpg'
+import mansion from '../../../assets/products/mansion.jpg'
+import moon from '../../../assets/products/moon.jpg'
+import nyapartment from '../../../assets/products/nyapartment.jpg'
+import broadway from '../../../assets/products/broadway.jpg'
+import jet from '../../../assets/products/gulfstream.jpg'
+import avatar from '../../../assets/products/avatar.jpg'
+
 
 export const productData = [
   { id: 0, img: milk, label: 'Milk', price: 1 },
@@ -25,31 +40,24 @@ export const productData = [
   { id: 7, img: rent, label: 'Monthly rent for 85 m2 (900 sqft)', price: 1600 },
   { id: 8, img: bengal, label: 'Bengal Cat', price: 5000 },
   { id: 9, img: wimbledon, label: 'Wimbledon Finals Tickets', price: 10000 },
-  { id: 10, img: '', label: 'Aged Whiskey', price: 11000 },
-  { id: 11, img: '', label: '??', price: 13000 },
-  { id: 12, img: '', label: '??', price: 18500 },
+  { id: 10, img: whiskey, label: 'Aged Whiskey', price: 11000 },
+  { id: 11, img: netflix, label: '78 Years Of Netflix Premium', price: 15000 },
+  { id: 12, img: collage, label: 'Public, out-of-state College, 4 years', price: 21184 },
   { id: 13, img: honda, label: 'Honda Civic', price: 26000 },
-  { id: 13, img: '', label: '??', price: 35000 },
-  { id: 14, img: audi, label: '2020 Audi TT', price: 50000 },
-  { id: 15, img: canalboat, label: 'Canal Boat', price: 100000 },
-  { id: 16, img: courtside, label: 'Lakers Court Side Season Tickets', price: 200000 },
-  { id: 17, img: '', label: '??', price: 0 },
-  { id: 18, img: '', label: '??', price: 0 },
-  { id: 19, img: '', label: '??', price: 0 },
-  { id: 20, img: '', label: '??', price: 0 },
-  { id: 21, img: yacht, label: 'Luxury Yacht', price: 14000000 },
-  { id: 22, img: '', label: '??', price: 0 },
-  { id: 23, img: '', label: '??', price: 0 },
-  { id: 24, img: '', label: '??', price: 0 },
-  { id: 25, img: '', label: '??', price: 0 },
-  { id: 26, img: '', label: '??', price: 0 },
-  { id: 27, img: '', label: '??', price: 0 },
-  { id: 28, img: '', label: '??', price: 0 },
-  { id: 29, img: '', label: '??', price: 0 },
-  { id: 30, img: '', label: '??', price: 0 },
-  { id: 31, img: '', label: '??', price: 0 },
-  { id: 32, img: '', label: '??', price: 0 },
-  { id: 33, img: '', label: '??', price: 0 },
-  { id: 34, img: '', label: '??', price: 0 },
-  { id: 35, img: '', label: '??', price: 0 },
+  { id: 14, img: penthouse, label: 'One Night at Luxury Penthouse Suite', price: 38000 },
+  { id: 15, img: audi, label: '2020 Audi TT', price: 50000 },
+  { id: 16, img: audi, label: '2020 Audi TT', price: 50000 },
+  { id: 17, img: canalboat, label: 'Canal Boat', price: 100000 },
+  { id: 18, img: courtside, label: 'Lakers Court Side Season Tickets', price: 200000 },
+  { id: 19, img: nyapartment, label: 'New York City Apartment', price: 1000000 },
+  { id: 20, img: jet, label: 'Gulfstream G150 Private Jet', price: 5000000 },
+  { id: 21, img: broadway, label: 'Broadway Production', price: 13000000 },
+  { id: 22, img: yacht, label: 'Luxury Yacht', price: 14000000 },
+  { id: 23, img: diamondshoes, label: 'The Passion Diamond Shoes', price: 17000000 },
+  { id: 24, img: moon, label: '1,000,000 Acres Off Lunar Land', price: 30000000 },
+  { id: 25, img: mansion, label: 'Beverly Hills Mansion', price: 56000000 },
+  { id: 26, img: avatar, label: 'Make The Avatar Movie', price: 237000000 },
+  { id: 27, img: island, label: 'Al Marjan Island', price: 462000000 },
+  { id: 28, img: luxembourg, label: 'Net Worth Of Luxembourg', price: 16000000000 },
+  { id: 29, img: iss, label: 'ISS Space Station', price: 150000000000 },
 ]
